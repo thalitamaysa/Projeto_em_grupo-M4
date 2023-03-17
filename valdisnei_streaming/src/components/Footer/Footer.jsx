745d0f6d2b7591effa34d41726cd7974d61eba44
lrmello <leonardo.reis28051995@gmail.com>
2023-03-17 17:07:10 -0300

    Iniciando footer e header com ajustes iniciais para inserir as imagens e ícones

---
+++ valdisnei_streaming/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.Footer}>&#169;Valdisnei. Todos os direitos reservados.</footer>
+  )
+}
