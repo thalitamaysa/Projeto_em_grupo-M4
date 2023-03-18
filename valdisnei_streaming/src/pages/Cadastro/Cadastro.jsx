4ecb947c85871de0ee1486a0570eecce2ee0320f
EmersonGald1n0 <emersonprof@outlook.com>
2023-03-18 16:41:49 -0300

    Add componente de cadastro + estilização

--- valdisnei_streaming/src/pages/Cadastro/Cadastro.jsx
+++ valdisnei_streaming/src/pages/Cadastro/Cadastro.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styles from './Cadastro.module.css'
+import logo_login from './../../assets/icons/logo_login.png'
+import user from './../../assets/icons/user.png'
+import { Link } from 'react-router-dom'
 
 export default function Cadastro() {
   return (
-    <div>Cadastro</div>
+    <div className={styles.container}>
+      <div className={styles.formulario}>
+        <form>
+          <img id='logo' src={logo_login} alt="logo_login" />
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Sobrenome"/>
+          {/* input de data de nascimento */}
+          <input type="text" placeholder="Data de nascimento"/>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Senha"/>
+          <input type="text" placeholder="Confirme sua senha"/>
+          <button>CADASTRAR</button>
+          <span id={styles.login}>Já é cadastrado? <Link className={styles.link} to='/login'>Login</Link></span>
+        </form>
+      </div>
+    </div>
   )
 }
