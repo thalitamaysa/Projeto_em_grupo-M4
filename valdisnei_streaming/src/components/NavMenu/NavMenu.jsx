5540c6fc140784bd126d6ee496c3576c41489d82
lrmello <leonardo.reis28051995@gmail.com>
2023-03-18 20:09:15 -0300

    Ajustes no header e realocação das imagens

--- valdisnei_streaming/src/components/NavMenu/NavMenu.jsx
+++ valdisnei_streaming/src/components/NavMenu/NavMenu.jsx
@@ -1,21 +1,47 @@
 import {Link} from "react-router-dom"
 import styles from "./NavMenu.module.css"
-import Logo from "../../assets/icons/logo.jpg"
+import Logo from "../../assets/icons/logo_header.png"
+import IconeInicio from "../../assets/icons/home.png"
+import IconeMusica from "../../assets/icons/musica.png"
+import IconeFilme from "../../assets/icons/filme.png"
+import IconeMinhaLista from "../../assets/icons/minhaLista.png"
+import IconeSeries from "../../assets/icons/tv.png"
 import React from 'react'
 
 export default function Nav() {
   return (
     <div>
       <nav className={styles.NavMenu}>
-        <img src={Logo} alt="" />
-        <Link to={"/"}>Inicio</Link>
-        <Link to={"/serie"}>Series</Link>
-        <Link to={"/filme"}>Filmes</Link>
-        <Link to={"/musica"}>Musicas</Link>
-        <Link to={"/"}>Minha Lista</Link>
+        <div className={styles.logo}>
+          <img src={Logo} alt="" className={`${styles.logo} ${styles.logoTop}`} />
+        </div>
+        
+
+        <div className={styles.linkContainer}>
+          <img src={IconeInicio} alt="" className={styles.icones}/>
+          <Link to={"/"} className={styles.links}>Inicio</Link>
+        </div>
+
+        <div className={styles.linkContainer}>
+          <img src={IconeSeries} alt="" className={styles.icones} />
+          <Link to={"/serie"} className={styles.links}>Series</Link>  
+        </div>  
+        
+        <div className={styles.linkContainer}>
+          <img src={IconeFilme} alt=""  className={styles.icones} />
+          <Link to={"/filme"} className={styles.links}>Filmes</Link>
+        </div>
+
+        <div className={styles.linkContainer}>
+          <img src={IconeMusica} alt=""  className={styles.icones}/>
+          <Link to={"/musica"} className={styles.links}>Musicas</Link>
+        </div>
+
+        <div className={styles.linkContainer}>
+          <img src={IconeMinhaLista} alt="" className={styles.icones} />
+          <Link to={"/"} className={styles.links}>Minha Lista</Link>
+        </div>
       </nav>
     </div>
   )
 }
-
-
