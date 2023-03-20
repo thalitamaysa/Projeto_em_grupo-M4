3a64a70abad422705ac134f4c812d573041c4b79
lrmello <leonardo.reis28051995@gmail.com>
2023-03-20 20:03:04 -0300

    Ajuste do header para dimensionar o header

--- valdisnei_streaming/src/components/NavMenu/NavMenu.jsx
+++ valdisnei_streaming/src/components/NavMenu/NavMenu.jsx
@@ -1,16 +1,28 @@
 import {Link} from "react-router-dom"
 import styles from "./NavMenu.module.css"
-import Logo from "../../assets/icons/logo_header.png"
+import Logo from "../../assets/icons/logo_header_menu.png"
 import IconeInicio from "../../assets/icons/home.png"
 import IconeMusica from "../../assets/icons/musica.png"
 import IconeFilme from "../../assets/icons/filme.png"
 import IconeMinhaLista from "../../assets/icons/minhaLista.png"
 import IconeSeries from "../../assets/icons/tv.png"
-import React from 'react'
+import IconeUser from "../../assets/icons/user.png"
+import React, { useState } from 'react'
 
 export default function Nav() {
+const [searchTerm, setSearchTerm] = useState("")
+
+/* Essa é uma função de callback para o usuário for digitar ela recebe um evento*/
+function handleSearchInputChange(e){
+  setSearchTerm(e.target.value)
+}
+/* Essa é uma função de callback quando o usuário envia os dados do formulário */
+function handleSearchSubmit(e){
+  e.preventDefault()
+}
+
   return (
-    <div>
+    <div className={styles.containerHeader}>
       <nav className={styles.NavMenu}>
         <div className={styles.logo}>
           <img src={Logo} alt="" className={`${styles.logo} ${styles.logoTop}`} />
@@ -41,7 +53,26 @@ export default function Nav() {
           <img src={IconeMinhaLista} alt="" className={styles.icones} />
           <Link to={"/"} className={styles.links}>Minha Lista</Link>
         </div>
+
+        <div className={styles.searchContainer}>
+        <form className={styles.handleSearch} onSubmit={handleSearchSubmit}>
+          <input 
+          type="text"
+          placeholder={`buscar...               \u{1F50D}`}
+          onChange = {handleSearchInputChange}
+          value={searchTerm}
+          className={styles.searchInput}
+          />
+        </form>
+        </div>
+
+        
       </nav>
+
+      
+      <div className={styles.userIcon}>
+          <img src={IconeUser}/>
+        </div>
     </div>
   )
 }
